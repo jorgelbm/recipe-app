cdc6df1f433ab56d430fc27ea83860c723e40719
Jorge Marques <jorge.bmarques1@gmail.com>
2022-03-16 18:20:21 -0300

    Estruturando pagina de almoço

--- src/routes/almoco.js
+++ src/routes/almoco.js
@@ -1,6 +1,18 @@
+import RecipesList from "../components/RecipesList"
+import styled from 'styled-components'
+
+const Titulo = styled.h1`
+        font-size: 2.5rem;
+        color: #2B2D42
+    `
+    
 export default function Almoco(){
 
+    
     return(
-        <h1>Almoço</h1>
+        <div>
+            <Titulo>Almoço</Titulo>
+            <RecipesList />
+        </div>
     )
 }
