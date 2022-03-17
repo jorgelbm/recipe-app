f686a4954ac63423d77f2a4adb9b0aef4770dec9
Jorge Marques <jorge.bmarques1@gmail.com>
2022-03-16 22:01:19 -0300

    Importando bootstrap-icons em index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import CafeDaManha from './routes/cafeDaManha'
 import Almoco from './routes/almoco';
